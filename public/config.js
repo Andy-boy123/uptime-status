a97526723e987af5cbe0629c1f320c5167ce7b74
Andy-boy123 <53182277+Andy-boy123@users.noreply.github.com>
2021-12-31 12:43:15 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -2,7 +2,7 @@
 window.Config = {
 
   // 站点名
-  SiteName: 'Uptime Status',
+  SiteName: 'Andy's服务器状态',
 
   // 站点链接
   SiteUrl: '/',
@@ -16,25 +16,26 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'm790207827-5d2450ad222851dcbaf4115e',
+    'm790207835-26c0b5d9cc217590a5addb89',
+    'm790207684-058babeb37e08e4dfe983d14',
+    'm790207681-086fbd7c3a89712191419e38',
+    'm790207831-f781308c1bec8328da9da2d9',
   ],
 
   // 是否显示监测站点的链接
-  ShowLink: true,
+  ShowLink: false,
 
   // 日志天数
   // 虽然免费版说仅保存60天日志，但测试好像API可以获取90天的
   // 不过时间不要设置太长，容易卡，接口请求也容易失败
-  CountDays: 60,
+  CountDays: 30,
 
   // 导航栏菜单
   Navi: [
     {
       text: 'Homepage',
-      url: 'https://status.org.cn/'
+      url: 'http://andylive.cn/'
     },
     {
       text: 'GitHub',
